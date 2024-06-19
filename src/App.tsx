4cb935210d4ff31f762e32b5eca653438eabca0d
Eder Henrique <ederhscc@gmail.com>
2024-06-18 21:47:16 -0300

    added structure of project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,44 @@
-import './App.css'
+// Components
+import { GrFormNext, GrFormPrevious } from "react-icons/gr";
+import UserForm from "./components/UserForm";
+import ReviewForm from "./components/ReviewForm";
+import Thanks from "./components/Thanks";
+
+// Hooks
+
+// CSS
+import "./App.css";
 
 function App() {
-  
   return (
-    <div className='app'>
-     <h1>Multistep Form with TS</h1>
+    <div className="app">
+      <div className="header">
+        <h2>Deixe sua avaliação</h2>
+        <p>
+          Ficamos felizes com a sua compra, utilize o formulário abaixo para
+          avaliar o produto.
+        </p>
+      </div>
+      <div className="form-container">
+        <p>passos</p>
+        <form>
+          <div className="inputs-container">
+            <UserForm/>
+          </div>
+          <div className="actions">
+            <button>
+              <GrFormPrevious />
+              <span>Voltar</span>
+            </button>
+            <button>
+              <span>Avançar</span>
+              <GrFormNext />
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
